7d7251d4d26c211765f15004e5e2b26535e1d4f1
josephdattilo03 <jadattilobusiness@gmail.com>
2025-04-22 00:26:21 -0400

    setup video session

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import mido
-import time
+from video_proc import list_macos_cameras, start_video_stream
 
 
 
 
 
-def main():
+def main(video_caputure_idx) :
 
-    note_on = mido.Message('note_on', note=70, velocity=64)
-    note_off = mido.Message('note_off', note=70, velocity=64)
-
-    note_on_2 = mido.Message('note_on', note=40, velocity=64)
-    note_off_2 = mido.Message('note_off', note=40, velocity=64)
-    outport = mido.open_output(mido.get_output_names()[0])    
-    while (True):
-        outport.send(note_on)
-        time.sleep(1)
-        outport.send(note_off)
-        outport.send(note_on_2)
-        time.sleep(1)
-        outport.send(note_off_2)
+    # note_on = mido.Message('note_on', note=70, velocity=64)
+    # note_off = mido.Message('note_off', note=70, velocity=64)
+    # note_on_2 = mido.Message('note_on', note=40, velocity=64)
+    # note_off_2 = mido.Message('note_off', note=40, velocity=64)
+    # outport = mido.open_output(mido.get_output_names()[0])    
+    start_video_stream(video_caputure_idx)
 
     
 
@@ -27,11 +20,15 @@ def main():
 if __name__ == "__main__":
 
     names = mido.get_output_names()
-    has_dark_energy = False
-    for name in names:
-        if name == "USB MIDI Dark Energy   USB MIDI Dark Energy":
-            has_dark_energy = True
-    if not has_dark_energy:
-        print("Dark Energy MIDI device not found")
-        exit()
-    main()
+    dark_energy_index = 0 # replace with -1 later 
+    # for i in range(len(names)):
+    #     if names[i] == "USB MIDI Dark Energy   USB MIDI Dark Energy":
+    #         dark_energy_index = i
+    #         break
+    # if dark_energy_index == -1:
+    #     print("Dark Energy MIDI device not found")
+    #     exit()
+    for idx, device in enumerate(list_macos_cameras()):
+        print(str(idx) + ": " + str(device))
+    chosen_device = input("Select a device from the list above: ")
+    main(int(chosen_device))
